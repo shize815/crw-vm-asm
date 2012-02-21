a6ba8c50d2d0f5ba493c34e51aabb3d24c2f3783
spin974@yahoo.fr <spin974@yahoo.fr@6212d2cd-a0cf-37d5-7447-1904c54ac794>
2012-02-21 16:52:46 +0000

    Instructions

---
+++ vm/include/vm_funcs.h
@@ -0,0 +1,16 @@
+/*
+** vm_funcs.h for corewar in /home/ecormi_p//crw-vm-asm/vm/vm_funcs
+** 
+** Made by pierre ecormier
+** Login   <ecormi_p@epitech.net>
+** 
+** Started on  Tue Feb 21 15:31:47 2012 pierre ecormier
+** Last update Tue Feb 21 15:39:28 2012 pierre ecormier
+*/
+
+#ifndef	__VM_FUNCS_H__
+#define	__VM_FUNCS_H__
+
+void	live(t_arena *arena, t_champ *champ, int *argv);
+
+#endif
---
+++ vm/vm_funcs/ops.c
@@ -0,0 +1,53 @@
+/*
+** live.c for corewar in /home/ecormi_p//crw-vm-asm/vm/vm_funcs
+** 
+** Made by pierre ecormier
+** Login   <ecormi_p@epitech.net>
+** 
+** Started on  Tue Feb 21 15:33:22 2012 pierre ecormier
+** Last update Tue Feb 21 16:40:08 2012 pierre ecormier
+*/
+
+#include	"op.h"
+#include	"corewar.h"
+#include	"vm_funcs.h"
+
+void		add(t_arena *arena, t_champ *champ, char type[4], int argv[4])
+{
+  champ->r[argv[2]] = champ->r[argv[0]] + champ->r[argv[1]];
+  champ->carry = (champ->r[argv[2]] == 0);
+  arena = arena;
+  type = type;
+}
+
+void		sub(t_arena *arena, t_champ *champ, char type[4], int argv[4])
+{
+  champ->r[argv[2]] = champ->r[argv[0]] - champ->r[argv[1]];
+  champ->carry = (champ->r[argv[2]] == 0);
+  arena = arena;
+  type = type;
+}
+
+void		and(t_arena *arena, t_champ *champ, char type[4], int argv[4])
+{
+  champ->r[argv[2]] = champ->r[argv[0]] & champ->r[argv[1]];
+  champ->carry = (champ->r[argv[2]] == 0);
+  arena = arena;
+  type = type;
+}
+
+void		or(t_arena *arena, t_champ *champ, char type[4], int argv[4])
+{
+  champ->r[argv[2]] = champ->r[argv[0]] | champ->r[argv[1]];
+  champ->carry = (champ->r[argv[2]] == 0);
+  arena = arena;
+  type = type;
+}
+
+void		xor(t_arena *arena, t_champ *champ, char type[4], int argv[4])
+{
+  champ->r[argv[2]] = champ->r[argv[0]] ^ champ->r[argv[1]];
+  champ->carry = (champ->r[argv[2]] == 0);
+  arena = arena;
+  type = type;
+}
