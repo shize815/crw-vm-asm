0f5ce086a617e11df4c17760b5aad138cb43db79
spin974@yahoo.fr <spin974@yahoo.fr@6212d2cd-a0cf-37d5-7447-1904c54ac794>
2012-02-21 14:28:29 +0000

    Structures

--- vm/include/corewar.h
+++ vm/include/corewar.h
@@ -5,27 +5,32 @@
 ** Login   <ecormi_p@epitech.net>
 ** 
 ** Started on  Mon Feb 13 16:32:13 2012 pierre ecormier
-** Last update Sun Feb 19 20:06:18 2012 pierre ecormier
+** Last update Tue Feb 21 15:22:00 2012 pierre ecormier
 */
 
 #ifndef	__COREWAR_H__
 #define	__COREWAR_H__
 
-#include "op.h"
-
-typedef	struct		s_asmline
+typedef	struct		s_champ
 {
-  unsigned char		bin[20];
-  int			size;
-  char			code;
-  char			type[MAX_ARGS_NUMBER];
-  char			*argv[MAX_ARGS_NUMBER];
-  struct s_asmline	*next;
-}			t_asmline;
+  char			*name;
+  char			*comment;
+  int			last_live;
+  int			nb_live;
+  int			timer;
+  int			pc;
+  int			r[16];
+  struct s_champ	*next;
+}			t_champ;
 
-int	is_label(char *str);
-int	write_file(t_asmline *line, char *header[2], int size, char *path);
-int	write_asm(t_asmline *line, char *path);
-int	parse_line(char *s, t_asmline *line, int ignore_flag);
+typedef	struct		s_arena
+{
+  int			cycle_to_die;
+  int			nb_live;
+  int			nb_process;
+  char			*map;
+}			t_arena;
+
+int			f(char *vm, t_champ *champ, int *argv);
 
 #endif
