13aa368015ee2baaed942e92a5cc462ff2f11ca1
spin974@yahoo.fr <spin974@yahoo.fr@6212d2cd-a0cf-37d5-7447-1904c54ac794>
2012-03-22 12:38:15 +0000

    Bugs ldi sti etc

--- vm/include/vm_funcs.h
+++ vm/include/vm_funcs.h
@@ -5,11 +5,16 @@
 ** Login   <ecormi_p@epitech.net>
 ** 
 ** Started on  Tue Feb 21 15:31:47 2012 pierre ecormier
-** Last update Thu Mar  8 16:54:32 2012 pierre ecormier
+** Last update Thu Mar 22 13:27:40 2012 pierre ecormier
 */
 
 #ifndef	__VM_FUNCS_H__
 #define	__VM_FUNCS_H__
+
+#include	"op.h"
+
+#define		VM_BORD(x)	((x) < 0 ? MEM_SIZE - (-(x) % MEM_SIZE) : (x) % MEM_SIZE)
+#define		REG_VALID(x)	((x) >= 0 && (x) < REG_NUMBER)
 
 void		live(t_arena *arena, t_champ *champ, char type[4], int argv[4]);
 void		ld(t_arena *arena, t_champ *champ, char type[4], int argv[4]);
