24537de7616c52e44c0af0ae37aa494f8db176ca
spin974@yahoo.fr <spin974@yahoo.fr@6212d2cd-a0cf-37d5-7447-1904c54ac794>
2012-03-23 14:48:30 +0000

    Hello

--- trunk/vm/vm_funcs/ops.c
+++ trunk/vm/vm_funcs/ops.c
@@ -5,7 +5,7 @@
 ** Login   <ecormi_p@epitech.net>
 ** 
 ** Started on  Tue Feb 21 15:33:22 2012 pierre ecormier
-** Last update Thu Mar 22 14:36:50 2012 pierre ecormier
+** Last update Fri Mar 23 15:40:09 2012 pierre ecormier
 */
 
 #include	<stdio.h>
@@ -15,6 +15,8 @@
 
 void		add(t_arena *arena, t_champ *champ, char type[4], int argv[4])
 {
+  if (!REG_VALID(argv[0]) || !REG_VALID(argv[1]))
+    return;
   printf("add %d %d\n", champ->r[argv[0]], champ->r[argv[1]]);
   champ->r[argv[2]] = champ->r[argv[0]] + champ->r[argv[1]];
   champ->carry = (champ->r[argv[2]] == 0);
@@ -24,6 +26,8 @@ void		add(t_arena *arena, t_champ *champ, char type[4], int argv[4])
 
 void		sub(t_arena *arena, t_champ *champ, char type[4], int argv[4])
 {
+  if (!REG_VALID(argv[0]) || !REG_VALID(argv[1]))
+    return;
   printf("sub %d %d\n", champ->r[argv[0]], champ->r[argv[1]]);
   champ->r[argv[2]] = champ->r[argv[0]] - champ->r[argv[1]];
   champ->carry = (champ->r[argv[2]] == 0);
@@ -33,6 +37,8 @@ void		sub(t_arena *arena, t_champ *champ, char type[4], int argv[4])
 
 void		and(t_arena *arena, t_champ *champ, char type[4], int argv[4])
 {
+  if (!REG_VALID(argv[0]) || !REG_VALID(argv[1]))
+    return;
   printf("and %d %d\n", champ->r[argv[0]], champ->r[argv[1]]);
   champ->r[argv[2]] = champ->r[argv[0]] & champ->r[argv[1]];
   champ->carry = (champ->r[argv[2]] == 0);
@@ -42,6 +48,8 @@ void		and(t_arena *arena, t_champ *champ, char type[4], int argv[4])
 
 void		or(t_arena *arena, t_champ *champ, char type[4], int argv[4])
 {
+  if (!REG_VALID(argv[0]) || !REG_VALID(argv[1]))
+    return;
   printf("or %d %d\n", champ->r[argv[0]], champ->r[argv[1]]);
   champ->r[argv[2]] = champ->r[argv[0]] | champ->r[argv[1]];
   champ->carry = (champ->r[argv[2]] == 0);
@@ -51,6 +59,8 @@ void		or(t_arena *arena, t_champ *champ, char type[4], int argv[4])
 
 void		xor(t_arena *arena, t_champ *champ, char type[4], int argv[4])
 {
+  if (!REG_VALID(argv[0]) || !REG_VALID(argv[1]))
+    return;
   printf("xor %d %d\n", champ->r[argv[0]], champ->r[argv[1]]);
   champ->r[argv[2]] = champ->r[argv[0]] ^ champ->r[argv[1]];
   champ->carry = (champ->r[argv[2]] == 0);
